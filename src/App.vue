ee4a19e3940a5b6d5dda1c4002683432a4661412
雷学伟 <leixw@helianhealth.com>
2018-11-23 14:56:31 +0800

    添加自定义地图打点功能。
    Signed-off-by: 雷学伟 <leixw@helianhealth.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="bg"
+         src="./assets/map.jpg">
+    <div ref="dragContaner"
+         class="drag-container">
+      <el-tooltip :key="item.id"
+                  effect="light"
+                  placement="top"
+                  v-for="item in nodes"
+                  :content="item.tooltip">
+        <div :data-id="item.id"
+             class="iconfont drag"
+             :data-x="item.position.x"
+             :data-y="item.position.y"
+             :style="getStyle(item.id)">
+          &#xe773;
+        </div>
+      </el-tooltip>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import interact from 'interactjs';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nodes: [
+        { id: 's1', tooltip: '天线 1', position: { x: 0, y: 0 } },
+        { id: 's2', tooltip: '天线 2', position: { x: 100, y: 50 } },
+        { id: 's3', tooltip: '天线 3', position: { x: 200, y: 100 } },
+        { id: 's4', tooltip: '天线 4', position: { x: 300, y: 150 } },
+        { id: 's5', tooltip: '天线 5', position: { x: 400, y: 200 } }
+      ]
+    };
+  },
+  mounted() {
+    this.setDragable('.drag', { context: this.$refs.dragContaner });
+  },
+  methods: {
+    getStyle(id) {
+      const item = this.nodes.find(v => v.id === id).position;
+      if (!item) {
+        return null;
+      }
+
+      const { x, y } = item;
+      return {
+        transform: `translate(${x / 100}rem, ${y / 100}rem)`
+      };
+    },
+    setDragable(...args) {
+      const self = this;
+
+      interact(...args).draggable({
+        onmove(event) {
+          const target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+          self.nodes.find(v => v.id === target.getAttribute('data-id')).position = { x, y };
+        }
+      })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  > .bg {
+    width: 19.2rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  .drag {
+    width: 0.24rem;
+    height: 0.24rem;
+    font-size: 0.24rem;
+    line-height: 0.24rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    color: blue;
+  }
 }
 </style>
